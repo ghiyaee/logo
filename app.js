7be543c6b68598c2091cde56e6cf73d7ac9a5177
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-18 23:10:37 +0330

    animation

---
+++ app.js
@@ -0,0 +1,75 @@
+const cir = document.querySelectorAll('.cir');
+const div=document.querySelector('.logo')
+console.log(cir);
+
+setTimeout(() => {
+    cir[0].innerHTML = 'J';
+    cir[0].style.animationName = 'none';
+    cir[0].style.backgroundColor="black"
+    cir[0].style.color = "white"
+    cir[0].style.border="none"
+}, 3000)
+setTimeout(() => {
+    cir[1].style.animationName = 'none';
+    cir[1].innerHTML = 'a';
+     cir[1].style.backgroundColor = "black"
+     cir[1].style.color = "white"
+     cir[1].style.border = "none"
+}, 4000)
+setTimeout(() => {
+    cir[2].style.animationName = 'none';
+    cir[2].innerHTML = 'v';
+     cir[2].style.backgroundColor = "black"
+     cir[2].style.color = "white"
+     cir[2].style.border = "none"
+}, 5000)
+setTimeout(() => {
+    cir[3].style.animationName = 'none';
+    cir[3].innerHTML = 'a';
+     cir[3].style.backgroundColor = "black"
+     cir[3].style.color = "white"
+     cir[3].style.border = "none"
+}, 6000)
+setTimeout(() => {
+    cir[4].style.animationName = 'none';
+    cir[4].innerHTML = 'S';
+     cir[4].style.backgroundColor = "black"
+     cir[4].style.color = "white"
+     cir[4].style.border = "none"
+}, 7000)
+setTimeout(() => {
+    cir[5].style.animationName = 'none';
+    cir[5].innerHTML = 'c';
+       cir[5].style.backgroundColor = "black"
+       cir[5].style.color = "white"
+       cir[5].style.border = "none"
+}, 8000)
+setTimeout(() => {
+    cir[6].style.animationName = 'none';
+    cir[6].innerHTML = 'r';
+       cir[6].style.backgroundColor = "black"
+       cir[6].style.color = "white"
+       cir[6].style.border = "none"
+}, 9000)
+setTimeout(() => {
+    cir[7].style.animationName = 'none';
+    cir[7].innerHTML = 'i';
+       cir[7].style.backgroundColor = "black"
+       cir[7].style.color = "white"
+       cir[7].style.border = "none"
+}, 10000)
+setTimeout(() => {
+    cir[8].style.animationName = 'none';
+    cir[8].innerHTML = 'p';
+       cir[8].style.backgroundColor = "black"
+       cir[8].style.color = "white"
+       cir[8].style.border = "none"
+}, 11000)
+setTimeout(() => {
+    cir[9].style.animationName = 'none';
+    cir[9].innerHTML = 't';
+    cir[9].style.backgroundColor = "black";
+    cir[9].style.color = "white";
+    cir[9].style.border = "none";
+    cir.style.textShadow = '1px 1px 5px 1px yellow';
+}, 12000)
