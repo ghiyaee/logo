112ea01c6d219e901982695cfb7858ff7d33ce1a
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-18 23:28:05 +0330

    fix animation

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ setTimeout(() => {
 }, 3000)
 setTimeout(() => {
     cir[1].style.animationName = 'none';
-    cir[1].innerHTML = 'a';
+    cir[1].innerHTML = 'A';
      cir[1].style.backgroundColor = "black"
      cir[1].style.color = "white"
      cir[1].style.border = "none"
 }, 4000)
 setTimeout(() => {
     cir[2].style.animationName = 'none';
-    cir[2].innerHTML = 'v';
+    cir[2].innerHTML = 'V';
      cir[2].style.backgroundColor = "black"
      cir[2].style.color = "white"
      cir[2].style.border = "none"
 }, 5000)
 setTimeout(() => {
     cir[3].style.animationName = 'none';
-    cir[3].innerHTML = 'a';
+    cir[3].innerHTML = 'A';
      cir[3].style.backgroundColor = "black"
      cir[3].style.color = "white"
      cir[3].style.border = "none"
@@ -39,37 +39,38 @@ setTimeout(() => {
 }, 7000)
 setTimeout(() => {
     cir[5].style.animationName = 'none';
-    cir[5].innerHTML = 'c';
+    cir[5].innerHTML = 'C';
        cir[5].style.backgroundColor = "black"
        cir[5].style.color = "white"
        cir[5].style.border = "none"
 }, 8000)
 setTimeout(() => {
     cir[6].style.animationName = 'none';
-    cir[6].innerHTML = 'r';
+    cir[6].innerHTML = 'R';
        cir[6].style.backgroundColor = "black"
        cir[6].style.color = "white"
        cir[6].style.border = "none"
 }, 9000)
 setTimeout(() => {
     cir[7].style.animationName = 'none';
-    cir[7].innerHTML = 'i';
+    cir[7].innerHTML = 'I';
        cir[7].style.backgroundColor = "black"
        cir[7].style.color = "white"
        cir[7].style.border = "none"
 }, 10000)
 setTimeout(() => {
     cir[8].style.animationName = 'none';
-    cir[8].innerHTML = 'p';
+    cir[8].innerHTML = 'P';
        cir[8].style.backgroundColor = "black"
        cir[8].style.color = "white"
        cir[8].style.border = "none"
 }, 11000)
 setTimeout(() => {
     cir[9].style.animationName = 'none';
-    cir[9].innerHTML = 't';
+    cir[9].innerHTML = 'T';
     cir[9].style.backgroundColor = "black";
     cir[9].style.color = "white";
     cir[9].style.border = "none";
-    cir.style.textShadow = '1px 1px 5px 1px yellow';
+    div.style.borderBottom = '1px solid yellow';
+
 }, 12000)
