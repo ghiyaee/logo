9ff7224afbc0c8701c2712bf5c8845839db1c58b
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-19 23:49:02 +0330

    fix js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const js = document.querySelector('.js');
 const line = document.querySelector('.line');
 let timer = 3000;
 const str = ['J', 'A', 'V', 'A', 'S', 'C', 'R', 'I', 'P', 'T'];
-let i = 0;
 for (let i = 0; i < circle.length;i++) {
     setTimeout(() => {
         circle[i].innerHTML = `${str[i]}`;
