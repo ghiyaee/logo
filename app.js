b32381aeef12bbee8e411798b8104a030e9006d6
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-19 14:46:09 +0330

    update js

--- app.js
+++ app.js
@@ -1,80 +1,31 @@
 const cir = document.querySelectorAll('.cir');
-const div=document.querySelector('.logo')
+const div = document.querySelector('.logo');
 const js = document.querySelector('.js');
-const line=document.querySelector('.line')
-console.log(js);
-   setTimeout(() => {
-       cir[0].innerHTML = 'J';
-       cir[0].style.animationName = 'none';
-       cir[0].style.backgroundColor = "black";
-       cir[0].style.color = "white";
-       cir[0].style.border = "none";
-   }, 3000)
-   setTimeout(() => {
-       cir[1].style.animationName = 'none';
-       cir[1].innerHTML = 'A';
-       cir[1].style.backgroundColor = "black";
-       cir[1].style.color = "white";
-       cir[1].style.border = "none";
-   }, 5000)
-   setTimeout(() => {
-       cir[2].style.animationName = 'none';
-       cir[2].innerHTML = 'V';
-       cir[2].style.backgroundColor = "black";
-       cir[2].style.color = "white";
-       cir[2].style.border = "none";
-   }, 7000)
-   setTimeout(() => {
-       cir[3].style.animationName = 'none';
-       cir[3].innerHTML = 'A';
-       cir[3].style.backgroundColor = "black";
-       cir[3].style.color = "white";
-       cir[3].style.border = "none";
-   }, 9000)
-   setTimeout(() => {
-       cir[4].style.animationName = 'none';
-       cir[4].innerHTML = 'S';
-       cir[4].style.backgroundColor = "black";
-       cir[4].style.color = "white";
-       cir[4].style.border = "none";
-   }, 11000)
-   setTimeout(() => {
-       cir[5].style.animationName = 'none';
-       cir[5].innerHTML = 'C';
-       cir[5].style.backgroundColor = "black";
-       cir[5].style.color = "white";
-       cir[5].style.border = "none";
-   }, 13000)
-   setTimeout(() => {
-       cir[6].style.animationName = 'none';
-       cir[6].innerHTML = 'R';
-       cir[6].style.backgroundColor = "black";
-       cir[6].style.color = "white";
-       cir[6].style.border = "none";
-   }, 15000)
-   setTimeout(() => {
-       cir[7].style.animationName = 'none';;
-       cir[7].innerHTML = 'I';
-       cir[7].style.backgroundColor = "black";
-       cir[7].style.color = "white";
-       cir[7].style.border = "none";
-   }, 17000)
-   setTimeout(() => {
-       cir[8].style.animationName = 'none';
-       cir[8].innerHTML = 'P';
-       cir[8].style.backgroundColor = "black";
-       cir[8].style.color = "white";
-       cir[8].style.border = "none";
-   }, 19000)
-   setTimeout(() => {
-       cir[9].style.animationName = 'none';
-       cir[9].innerHTML = 'T';
-       cir[9].style.backgroundColor = "black";
-       cir[9].style.color = "white";
-       cir[9].style.border = "none";
-       div.style.borderBottom = '1px solid yellow';
-       js.classList.remove('hiden')
-       line.classList.add('hiden')
-   }, 21000)
+const line = document.querySelector('.line');
+
+let timer = 3000;
+const str = ['J', 'A', 'V', 'A', 'S', 'C', 'R', 'I', 'P', 'T'];
+for (let i in cir) {
+    setTimeout(() => {
+        cir[i].innerHTML = `${str[i]}`;
+        cir[i].style.animationName = 'none';
+        cir[i].style.backgroundColor = "black";
+        cir[i].style.color = "white";
+        cir[i].style.border = "none";
+        if (i >= str.length - 1) {
+            div.style.borderBottom = '1px solid yellow';
+            js.classList.remove('hiden');
+            line.classList.add('hiden');
+        }
+    }, timer);
+    timer += 2000; 
+}
+    
+
+
+
+
+
+
 
 
