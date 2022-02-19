8e10842e772f4c9286f836c9e659819a12fe095f
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-19 15:20:14 +0330

    change names

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const cir = document.querySelectorAll('.cir');
-const div = document.querySelector('.logo');
+const circle = document.querySelectorAll('.circle');
+const logo = document.querySelector('.logo');
 const js = document.querySelector('.js');
 const line = document.querySelector('.line');
-
+console.log(typeof circle);
 let timer = 3000;
 const str = ['J', 'A', 'V', 'A', 'S', 'C', 'R', 'I', 'P', 'T'];
-for (let i in cir) {
+for (let i in circle) {
     setTimeout(() => {
-        cir[i].innerHTML = `${str[i]}`;
-        cir[i].style.animationName = 'none';
-        cir[i].style.backgroundColor = "black";
-        cir[i].style.color = "white";
-        cir[i].style.border = "none";
+        circle[i].innerHTML = `${str[i]}`;
+        circle[i].style.animationName = "none";
+        circle[i].style.backgroundColor = "black";
+        circle[i].style.color = "white";
+        circle[i].style.border = "none";
         if (i >= str.length - 1) {
-            div.style.borderBottom = '1px solid yellow';
+            logo.style.borderBottom = '1px solid yellow';
             js.classList.remove('hiden');
             line.classList.add('hiden');
         }
