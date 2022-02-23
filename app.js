5918636d229f222e551c05c82de65c774b540089
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-02-23 12:55:36 +0330

    fix bugs

--- app.js
+++ app.js
@@ -19,32 +19,32 @@ form.addEventListener('submit', (e) => {
     int.value = '';
     localStorage.setItem('arry', JSON.stringify(arry));
 })
-const conv = JSON.parse(localStorage.getItem('arry', arry));
+const localName = JSON.parse(localStorage.getItem('arry', arry));
 
 ////////////////////////////////////////////first  one
-circle.forEach((p, index, arr) => {
-       const color1 = Math.floor(Math.random() * 255);
-       const color2 = Math.floor(Math.random() * 255);
-       const color3 = Math.floor(Math.random() * 255);
-      arr[index].style.backgroundColor = `rgb(${color1},${color2},${color3})`;
-    if (conv[index] != undefined) {
-        setTimeout(() => {
-            arr[index].innerHTML = `${conv[index]}`;
-            arr[index].style.animationName = "none";
-            arr[index].style.backgroundColor = "black";
-            arr[index].style.color = "white";
-            arr[index].style.border = "none";
-            if (index == conv.length-1) {
-                logo.style.borderBottom = '1px solid yellow';
-                js.classList.remove('hiden');
-                line.classList.add('hiden');
-            }
-        }, timer);
-        timer += 2000;
-    } else {
-        return
-    }
-  })
+// circle.forEach((p, index, arr) => {
+//        const color1 = Math.floor(Math.random() * 255);
+//        const color2 = Math.floor(Math.random() * 255);
+//        const color3 = Math.floor(Math.random() * 255);
+//       arr[index].style.backgroundColor = `rgb(${color1},${color2},${color3})`;
+//     if (localName[index] != undefined) {
+//         setTimeout(() => {
+//             arr[index].innerHTML = `${localName[index]}`;
+//             arr[index].style.animationName = "none";
+//             arr[index].style.backgroundColor = "black";
+//             arr[index].style.color = "white";
+//             arr[index].style.border = "none";
+//             if (index == localName.length-1) {
+//                 logo.style.borderBottom = '1px solid yellow';
+//                 js.classList.remove('hiden');
+//                 line.classList.add('hiden');
+//             }
+//         }, timer);
+//         timer += 2000;
+//     } else {
+//         return
+//     }
+//   })
 
 
 ///////////////////////////////////////////secound two
@@ -53,43 +53,47 @@ circle.forEach((p, index, arr) => {
 //        const color1 = Math.floor(Math.random() * 255);
 //        const color2 = Math.floor(Math.random() * 255);
 //        const color3 = Math.floor(Math.random() * 255);
-//        circle[i].style.backgroundColor = `rgb(${color1},${color2},${color3})`;
-//      setTimeout(() => {
-//         circle[i].innerHTML = `${conv[i]}`;
-//         circle[i].style.animationName = "none";
-//         circle[i].style.backgroundColor = "black";
-//         circle[i].style.color = "white";
-//         circle[i].style.border = "none";
-//         if (i >= str.length - 1) {
-//             logo.style.borderBottom = '1px solid yellow';
-//             js.classList.remove('hiden');
-//             line.classList.add('hiden');
-//         }
-//     }, timer);
-//     timer += 2000;
-// };
-
-///////////////////////////////////////////third three
-
-// let j = 0;
-//   for (let i of circle) {
-//           const color1 = Math.floor(Math.random() * 255);
-//           const color2 = Math.floor(Math.random() * 255);
-//           const color3 = Math.floor(Math.random() * 255);
-//          i.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
-//       setTimeout(() => {
-//           i.innerHTML = `${conv[j]}`;
-//           j++;
-//             i.style.animationName = "none";
-//             i.style.backgroundColor = "black";
-//             i.style.color = "white";
-//             i.style.border = "none";
-//             if (j >= str.length - 1) {
+//     circle[i].style.backgroundColor = `rgb(${color1},${color2},${color3})`;
+//     if (localName[i] != undefined) {
+//         setTimeout(() => {
+//             circle[i].innerHTML = `${localName[i]}`;
+//             circle[i].style.animationName = "none";
+//             circle[i].style.backgroundColor = "black";
+//             circle[i].style.color = "white";
+//             circle[i].style.border = "none";
+//             if (i >= localName.length - 1) {
 //                 logo.style.borderBottom = '1px solid yellow';
 //                 js.classList.remove('hiden');
 //                 line.classList.add('hiden');
 //             }
 //         }, timer);
+//         timer += 2000;
+//     };   
+// };
+
+///////////////////////////////////////////third three
+
+let j = 0;
+  for (let i of circle) {
+          const color1 = Math.floor(Math.random() * 255);
+          const color2 = Math.floor(Math.random() * 255);
+          const color3 = Math.floor(Math.random() * 255);
+      i.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
+      if (localName[j] != undefined) {
+          setTimeout(() => {
+              i.innerHTML = `${localName[j]}`;
+              j++;
+              i.style.animationName = "none";
+              i.style.backgroundColor = "black";
+              i.style.color = "white";
+              i.style.border = "none";
+              if (j >= str.length - 1) {
+                  logo.style.borderBottom = '1px solid yellow';
+                  js.classList.remove('hiden');
+                  line.classList.add('hiden');
+              }
+          }, timer);
        
-//       timer += 2000;
-//     }
+          timer += 2000;
+      };
+};
